7eca300d7608354b383ee9ca2a61e3f8269a6a23
iankeliher <iankeliher@gmail.com>
2022-12-16 17:58:16 -0500

    Create homepage.js

---
+++ js/homepage.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+	console.log("this works");
+	// $('#stripe-text').hover(function() {
+	// 	$(this).toggleClass('black-text');
+	// });
+
+	// $("#stripe-text").toggleClass('black-text');
+
+});
